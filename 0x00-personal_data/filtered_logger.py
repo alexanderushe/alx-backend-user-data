3939d6902194e58c49c98ae14bed0f6f280458f3
alexanderushe <alex.f.ushe@gmail.com>
2023-11-16 05:00:55 +0200

    pii fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import logging
 import re
 from typing import List
 
-
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str
         ) -> str:
@@ -15,6 +15,19 @@ def filter_datum(
     return re.sub(
         fr'({separator.join(fields)}){separator}',
         f'{redaction}{separator}', message)
+
+
+def get_logger() -> logging.Logger:
+    '''creates a new logger for user data
+    '''
+    logger = logging.getLogger("user_data")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(stream_Handler)
+    return logger
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
